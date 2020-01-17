208ef6a0a2b7a9601b081dd0c3a4cb664029f47e
Lauren Larios <54549719+laurrennaa@users.noreply.github.com>
2020-01-17 13:33:32 -0800

    Add files via upload

---
+++ stranger things.rb
@@ -0,0 +1,23 @@
+# Welcome to Sonic Pi v3.1
+use_bpm 160
+
+live_loop :first_loop do
+  use_synth :saw
+  play 60
+  sleep 0.5
+  play 64
+  sleep 0.5
+  play 67
+  sleep 0.5
+  play 71
+  sleep 0.5
+  play 72
+  sleep 0.5
+  play 71
+  sleep 0.5
+  play 67
+  sleep 0.5
+  play 64
+  sleep 0.5
+end
+
